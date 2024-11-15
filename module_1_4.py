56a847374cfc3bc2a93d60bdcb447242e05a1243
Ramazan <ramgus@gmail.com>
2024-11-15 06:33:34 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+my_string = input("Ввод в консоль: ") # Ввводим в консоль "Я изучаю Python в университете Urban"
+kol= "Количество символов введенного текста:"
+print(kol,len(my_string))
+kons= "Вывод на консоль:"
+print ( kons, my_string.upper()) # "Вывод строки в верхнем регистре"
+print("Вывод строки в нижнем регистре: ",my_string.lower())         # Вывод строки в верхнем регистре
+print("Изменение строки my_string, удалив все пробелы: ", my_string.replace(" ",""))
+print("Вывод первого символа строки my_string: ",my_string[0])
+print("Вывод последнего символа строки my_string: ",my_string[-1])
+
+
+
