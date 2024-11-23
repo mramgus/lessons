e7486aa4d5715563e5d4b97c4c6707d985ee1ff2
Ramazan <ramgus@gmail.com>
2024-11-23 10:11:15 +0300

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,26 @@
+numers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+n = 0
+print('Исходный код:')
+print('numers =', numers)
+primes = []
+not_primes = []
+i = 0
+for i in range(len(numers)):
+    is_prime = True
+    n = numers [i]
+    if n < 2:
+        continue
+    else:
+        f = n**(1/2)
+        for a in range(2, int(f+1)):
+            if n% a == 0:
+                is_prime = False
+                break
+    if not (is_prime):
+        not_primes.append(n)
+    else:
+        primes.append(n)
+is_prime = True
+print('Вывод на консоль:')
+print('Primes:', primes)
+print('Not Primes:', not_primes)
