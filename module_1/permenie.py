84863840879d40f6071bae74bb457af0beddea13
Ramazan <ramgus@gmail.com>
2024-12-03 23:23:23 +0300

    Update and rename permenie.py to module_1/permenie.py

---
+++ module_1/permenie.py
@@ -0,0 +1,5 @@
+kol_dz=12
+kol_zat_chasov= 1.5
+nazvanie_kursa = "Python"
+zatracheno_chasov = 1.5/12
+print ("Курс:",nazvanie_kursa,",","всего задач:",kol_dz,",",'затрачено часов:',kol_zat_chasov,",", "среднее время выполнения", zatracheno_chasov, "часа.")
